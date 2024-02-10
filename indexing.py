1d68596452094d19d9e6ee6516f44ab778861374
darrennguyen25 <darrennguyen@cpp.edu>
2024-02-10 00:32:49 -0800

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Darren Nguyen
 # FILENAME: indexing.py
-# SPECIFICATION: Output tf-idf document-term matrix
+# SPECIFICATION: Output tf-idf document-term matrix from a collection of documents
 # FOR: CS 4250- Assignment #1
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -39,23 +39,28 @@ for i in range(len(documents)):
     documents[i] = ' '.join([documents[i].replace(w, stemming[w]) for w in stemming if w in documents[i].split()])
 print("stemming removal", documents)
 
-#Identifying the index terms.
-#--> add your Python code here
-terms = list({w for d in documents for w in d.split()})
+terms = []
+[terms.append(w) for d in documents for w in d.split(' ') if w not in terms]
 print("index terms:", terms)
 
 #Building the document-term matrix by using the tf-idf weights.
 #--> add your Python code here
 docTermMatrix = []
 
-tf = [{w: d.split().count(w) for w in set(d.split())} for d in documents]
-print("tf for documents:", tf)
+def findtf(t, d):
+    return d.split(' ').count(t)/len(d.split(' '))
 
-idf = []
-for w in terms:
-    df = sum([1 for d in documents if w in d.split()])
-    idf += [math.log10(len(documents)/df)]
-print("idf for terms:", idf)
+def findidf(t, D):
+    df = sum([1 for d in D if t in d])
+    return math.log10(len(D)/df)
+
+def findtfidf(t, d, D):
+    return round((findtf(t, d) * findidf(t, D)), 3)
+
+docTermMatrix = [[findtfidf(t, d, documents) for t in terms] for d in documents]
 
 #Printing the document-term matrix.
 #--> add your Python code here
+print("Document-Term Matrix:")
+for d in docTermMatrix:
+    print(d)
